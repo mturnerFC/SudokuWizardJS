031ef37e904b56214940dbc747436017fca29521
mturnerFC <mark_turner@comcast.net>
2021-01-25 19:45:31 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 var SudokuWizard;
 window.onload = function() {
     const Model = new model();
@@ -24,6 +19,8 @@ window.onload = function() {
     
     const display = new Display(Model);
     display.ClearDocs();
+  
+    const bsc = new ButtonStateControl(Model);
+    bsc.Initialize(Model);
 };
-
 
